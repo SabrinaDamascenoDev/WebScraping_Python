15f9496bacc4e58ff5b3d3a86479b646bc1da449
SabrinaDamascenoDev <sabrinadamascenodev@gmail.com>
2024-12-15 11:15:08 -0300

    Aprendendo a juntar informaçoes dentro de dicionarios

--- src/main.py
+++ src/main.py
@@ -21,3 +21,42 @@ print(scraped.find("article", class_="product_pod").h3.a.text)
 #Pega o nome td do livro pq ele ta dentro do atributo title da tag a
 print(scraped.find("article", class_="product_pod").h3.a["title"])
 
+elements = scraped.findAll("article", class_="product_pod")
+
+for itens in elements:
+    print(itens.h3.a["title"])
+#Pegar os precos que estao em uma tag p com a classe price_color
+prices = scraped.findAll("p", class_="price_color")
+
+for price in prices:
+    # Tirar um element
+    price = float(price.text.lstrip("£"))
+    print(price)
+    print(price)
+
+results = []
+
+for element in elements:
+    results.append(element.h3.a['title'])
+
+resultsPrice = []
+
+for price in prices:
+    price = float(price.text.lstrip("£"))
+    resultsPrice.append(price)
+
+print(list(zip(results, resultsPrice)))
+
+resul = []
+articles = scraped.findAll("article", class_="product_pod")
+
+for article in articles:
+    nome = article.h3.a['title']
+    preco = article.find("div", class_="product_price").find("p", class_="price_color")
+    preco = float(preco.text.lstrip("£"))
+    dicDosDois = {nome: preco}
+    resul.append(dicDosDois)
+
+print(resul)
+
+print(scraped.select(".product_price .price_color"))
